720cdc630fdb5436ee07aa58d0b3de40571fa6bd
Teemo Tebest <teemo.tebest@gmail.com>
2026-01-22 11:55:18 +0100

    Updated

--- src/jsx/Footer.jsx
+++ src/jsx/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
             <div className="footer_content anchor_videos" id="anchor_videos">
               <h3>Watch the videos</h3>
               <div className="iframe_container youtube_iframe">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/x1cQfSEXUN8?si=aMwyDCUdUSMhohR_" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen />
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/x1cQfSEXUN8" title="Commodities and Development Report" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen />
               </div>
               <ul className="hidden">
                 <li>
